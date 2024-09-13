584e977930dbeb8ca1bd803fd042bc9e9009189c
Danial <mdanialrma@gmail.com>
2024-09-13 13:42:53 +0700

    test: more test cases for WithValue and Context

--- context_test.go
+++ context_test.go
@@ -7,10 +7,26 @@ import (
 )
 
 func TestContext(t *testing.T) {
+	// even with the same underlying empty struct, Go will still distinguish between
+	//  each type declaration
+	type customType struct{}
+	type anotherCustomType struct{}
+	var customTypeKey customType
+	var anotherCustomTypeKey anotherCustomType
+
 	httpCtx := Background()
 	httpCtx.WithValue("Hello", "world")
 	httpCtx.WithValue("Hi", "Goravel")
+	httpCtx.WithValue(customTypeKey, "halo")
+	httpCtx.WithValue(anotherCustomTypeKey, "hola")
+	httpCtx.WithValue(1, "one")
+	httpCtx.WithValue(2.2, "two point two")
+
 	ctx := httpCtx.Context()
 	assert.Equal(t, ctx.Value("Hello").(string), "world")
 	assert.Equal(t, ctx.Value("Hi").(string), "Goravel")
+	assert.Equal(t, "halo", ctx.Value(customTypeKey))
+	assert.Equal(t, "hola", ctx.Value(anotherCustomTypeKey))
+	assert.Equal(t, "one", ctx.Value(1))
+	assert.Equal(t, "two point two", ctx.Value(2.2))
 }
