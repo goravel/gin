25ba222451d3e930261ae6f816f6d694d5a84d00
Evgeny <113383200+KlassnayaAfrodita@users.noreply.github.com>
2024-10-11 18:35:30 +0300

    Update route.go

--- route.go
+++ route.go
@@ -30,6 +30,7 @@ func NewRoute(config config.Config, parameters map[string]any) (*Route, error) {
 	engine := gin.New()
 	engine.MaxMultipartMemory = int64(config.GetInt("http.drivers.gin.body_limit", 4096)) << 10
 	engine.Use(gin.Recovery()) // recovery middleware
+	engine.Use(gin.WithTimeout(time.Duration(config.GetInt("http.request_timeout", 3) * time.Second) // timeout middleware
 	if debugLog := getDebugLog(config); debugLog != nil {
 		engine.Use(debugLog)
 	}
