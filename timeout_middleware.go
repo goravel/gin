c07c05d5c38d88228db13b4f23c853760371ee42
Evgeny <113383200+KlassnayaAfrodita@users.noreply.github.com>
2024-10-15 15:13:41 +0300

    Update timeout_middleware.go

--- timeout_middleware.go
+++ timeout_middleware.go
@@ -12,7 +12,7 @@ import (
 // TimeoutMiddleware creates middleware to set a timeout for a request
 func TimeoutMiddleware() contractshttp.Middleware {
 	return func(ctx contractshttp.Context) {
-		timeout := config.GetInt("http.http.drivers.gin.timeout", 3) * time.Second
+		timeout := config.GetInt("http.timeout_request", 3) * time.Second
 		timeoutCtx, cancel := context.WithTimeout(ctx.Context(), timeout)
 		defer cancel()
 
