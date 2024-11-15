cc7a2615462bd0d8ca9c4e69414d8e2ab47d5a19
Evgeny <113383200+KlassnayaAfrodita@users.noreply.github.com>
2024-11-16 02:59:08 +0300

    Update gin.go

--- facades/gin.go
+++ facades/gin.go
@@ -1,25 +1,27 @@
 package facades
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/goravel/framework/contracts/route"
-
 	"github.com/goravel/gin"
 )
 
 func Route(driver string) route.Route {
-	if  gin.App != nil {
+	if gin.App != nil {
 		instance, err := gin.App.MakeWith(gin.RouteBinding, map[string]any{
 			"driver": driver,
 		})
+
+		if err != nil {
+			log.Fatalln(err)
+			return nil
+		}
+
+		return instance.(*gin.Route)
 	} else {
-    		fmt.Println("App is nil")
-	}
-	if err != nil {
-		log.Fatalln(err)
+		fmt.Println("App is nil")
 		return nil
 	}
-
-	return instance.(*gin.Route)
 }
