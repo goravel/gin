0e340135ef5211e957fa9cc085af01f3c7d5cf1f
Evgeny <113383200+KlassnayaAfrodita@users.noreply.github.com>
2024-12-14 01:34:09 +0300

    Update middleware_timeout.go

--- middleware_timeout.go
+++ middleware_timeout.go
@@ -11,31 +11,38 @@ import (
 
 // Timeout creates middleware to set a timeout for a request
 func Timeout(timeout time.Duration) contractshttp.Middleware {
-	return func(ctx contractshttp.Context) {
-		timeoutCtx, cancel := context.WithTimeout(ctx.Context(), timeout)
-		defer cancel()
+    return func(ctx contractshttp.Context) {
+        timeoutCtx, cancel := context.WithTimeout(ctx.Context(), timeout)
+        defer cancel()
 
-		ctx.WithContext(timeoutCtx)
+        ctx.WithContext(timeoutCtx)
 
-		done := make(chan struct{})
+        done := make(chan struct{})
 
-		go func() {
-           	 	defer func() {
-                		if r := recover(); r != nil {
-                    			ctx.Request().AbortWithStatusJson(http.StatusInternalServerError, map[string]interface{}{"error": "Internal Server Error"})
-                		}
-                		close(done)
-            		}()
+        go func() {
+            defer close(done)
 
-            		ctx.Request().Next()
-        	}()
+            defer func() {
+                if err := recover(); err != nil {
+                    if globalRecoverCallback != nil {
+                        globalRecoverCallback(ctx.Context(), err)
+                    } else {
+                        ctx.Request().AbortWithStatusJSON(http.StatusInternalServerError, map[string]interface{}{
+                            "error": "Internal Server Error",
+                        })
+                    }
+                }
+            }()
 
-		select {
-		case <-done:
-		case <-ctx.Request().Origin().Context().Done():
-			if errors.Is(ctx.Request().Origin().Context().Err(), context.DeadlineExceeded) {
-				ctx.Request().AbortWithStatus(http.StatusGatewayTimeout)
-			}
-		}
-	}
+            ctx.Request().Next()
+        }()
+
+        select {
+        case <-done:
+        case <-ctx.Request().Origin().Context().Done():
+            if errors.Is(ctx.Request().Origin().Context().Err(), context.DeadlineExceeded) {
+                ctx.Request().AbortWithStatus(http.StatusGatewayTimeout)
+            }
+        }
+    }
 }
