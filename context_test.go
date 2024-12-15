b325e9c6ab057d5145d09a63f8968dd59982e82f
Wenbo Han <hwbrzzl@gmail.com>
2024-12-15 16:50:35 +0800

    fix: Context method (#115)
    * fix: Context method
    
    * fix test
    
    * update test
    
    * update test

--- context_test.go
+++ context_test.go
@@ -1,32 +1,48 @@
 package gin
 
 import (
+	"context"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestContext(t *testing.T) {
-	// even with the same underlying empty struct, Go will still distinguish between
-	//  each type declaration
 	type customType struct{}
-	type anotherCustomType struct{}
 	var customTypeKey customType
-	var anotherCustomTypeKey anotherCustomType
 
-	httpCtx := Background()
+	ginCtx, _ := gin.CreateTestContext(httptest.NewRecorder())
+	ginCtx.Request = httptest.NewRequest("GET", "/", nil)
+	httpCtx := NewContext(ginCtx)
 	httpCtx.WithValue("Hello", "world")
 	httpCtx.WithValue("Hi", "Goravel")
 	httpCtx.WithValue(customTypeKey, "halo")
-	httpCtx.WithValue(anotherCustomTypeKey, "hola")
+
+	userContext := context.Background()
+	//nolint:all
+	userContext = context.WithValue(userContext, "user_a", "b")
+	httpCtx.WithContext(userContext)
+
 	httpCtx.WithValue(1, "one")
 	httpCtx.WithValue(2.2, "two point two")
 
+	assert.Equal(t, "world", httpCtx.Value("Hello"))
+	assert.Equal(t, "Goravel", httpCtx.Value("Hi"))
+	assert.Equal(t, "halo", httpCtx.Value(customTypeKey))
+	assert.Equal(t, "one", httpCtx.Value(1))
+	assert.Equal(t, "two point two", httpCtx.Value(2.2))
+
+	// The value of UserContext can't be covered.
+	assert.Equal(t, "b", httpCtx.Value("user_a"))
+
 	ctx := httpCtx.Context()
+
 	assert.Equal(t, "world", ctx.Value("Hello"))
 	assert.Equal(t, "Goravel", ctx.Value("Hi"))
 	assert.Equal(t, "halo", ctx.Value(customTypeKey))
-	assert.Equal(t, "hola", ctx.Value(anotherCustomTypeKey))
+	assert.Equal(t, "b", ctx.Value("user_a"))
 	assert.Equal(t, "one", ctx.Value(1))
 	assert.Equal(t, "two point two", ctx.Value(2.2))
 }
