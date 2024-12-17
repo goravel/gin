04abc74170c5e09adc6578b9902909c22b4b20fe
耗子 <i@haozi.net>
2024-12-17 15:24:07 +0800

    feat: add Listen method (#117)
    * feat: add Listen method
    
    * feat: optimize code
    
    * fix: test
    
    * fix: test
    
    * feat: add ListenTLSWithCert
    
    * fix: test

--- route.go
+++ route.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"net"
 	"net/http"
 	"net/http/httptest"
 	"time"
@@ -88,6 +89,44 @@ func (r *Route) GlobalMiddleware(middlewares ...httpcontract.Middleware) {
 	r.setMiddlewares(middlewares)
 }
 
+func (r *Route) Listen(l net.Listener) error {
+	r.outputRoutes()
+	color.Green().Println(termlink.Link("[HTTP] Listening and serving HTTP on", "http://"+l.Addr().String()))
+
+	r.server = &http.Server{
+		Addr:           l.Addr().String(),
+		Handler:        http.AllowQuerySemicolons(r.instance),
+		MaxHeaderBytes: r.config.GetInt("http.drivers.gin.header_limit", 4096) << 10,
+	}
+
+	if err := r.server.Serve(l); !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	return nil
+}
+
+func (r *Route) ListenTLS(l net.Listener) error {
+	return r.ListenTLSWithCert(l, r.config.GetString("http.tls.ssl.cert"), r.config.GetString("http.tls.ssl.key"))
+}
+
+func (r *Route) ListenTLSWithCert(l net.Listener, certFile, keyFile string) error {
+	r.outputRoutes()
+	color.Green().Println(termlink.Link("[HTTPS] Listening and serving HTTPS on", "https://"+l.Addr().String()))
+
+	r.tlsServer = &http.Server{
+		Addr:           l.Addr().String(),
+		Handler:        http.AllowQuerySemicolons(r.instance),
+		MaxHeaderBytes: r.config.GetInt("http.drivers.gin.header_limit", 4096) << 10,
+	}
+
+	if err := r.tlsServer.ServeTLS(l, certFile, keyFile); !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	return nil
+}
+
 func (r *Route) Run(host ...string) error {
 	if len(host) == 0 {
 		defaultHost := r.config.GetString("http.host")
@@ -108,11 +147,11 @@ func (r *Route) Run(host ...string) error {
 		MaxHeaderBytes: r.config.GetInt("http.drivers.gin.header_limit", 4096) << 10,
 	}
 
-	if err := r.server.ListenAndServe(); errors.Is(err, http.ErrServerClosed) {
-		return nil
-	} else {
+	if err := r.server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
 		return err
 	}
+
+	return nil
 }
 
 func (r *Route) RunTLS(host ...string) error {
@@ -149,11 +188,11 @@ func (r *Route) RunTLSWithCert(host, certFile, keyFile string) error {
 		MaxHeaderBytes: r.config.GetInt("http.drivers.gin.header_limit", 4096) << 10,
 	}
 
-	if err := r.tlsServer.ListenAndServeTLS(certFile, keyFile); errors.Is(err, http.ErrServerClosed) {
-		return nil
-	} else {
+	if err := r.tlsServer.ListenAndServeTLS(certFile, keyFile); !errors.Is(err, http.ErrServerClosed) {
 		return err
 	}
+
+	return nil
 }
 
 func (r *Route) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
